0228850a1ebf67717c528ef36b0bd6f517a257c1
hieuqautonomous <hieu.q@autonomous.nyc>
2018-06-01 14:08:00 +0700

    add cc and bcc

--- mail-service/main/web.go
+++ mail-service/main/web.go
@@ -16,7 +16,6 @@ import (
 	"strings"
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
-	"errors"
 )
 
 func main() {
@@ -45,17 +44,130 @@ func main() {
 			context.JSON(http.StatusOK, result)
 		})
 		index.POST("/", func(context *gin.Context) {
-			from := context.Request.PostFormValue("from")
-			to := context.Request.PostFormValue("to")
-			subject := context.Request.PostFormValue("subject")
-			body := context.Request.PostFormValue("body")
-			err = Send(from, to, subject, body, context)
+			form, err := context.MultipartForm()
 			if err != nil {
-				log.Print(err)
+				log.Println(err)
 				context.JSON(http.StatusOK, gin.H{
 					"status":  -1,
 					"message": err.Error(),
 				})
+				return
+			}
+
+			from := context.Request.PostFormValue("from")
+			if len(from) <= 0 {
+				context.JSON(http.StatusOK, gin.H{
+					"status":  -1,
+					"message": "from is invalid",
+				})
+				return
+			}
+			to, ok := form.Value["to[]"]
+			if !ok {
+				to = []string{}
+			}
+			if len(to) <= 0 {
+				context.JSON(http.StatusOK, gin.H{
+					"status":  -1,
+					"message": "to is invalid",
+				})
+				return
+			}
+			cc, ok := form.Value["cc[]"]
+			if !ok {
+				cc = []string{}
+			}
+			bcc, ok := form.Value["bcc[]"]
+			if !ok {
+				bcc = []string{}
+			}
+			subject := context.Request.PostFormValue("subject")
+			if len(subject) <= 0 {
+				context.JSON(http.StatusOK, gin.H{
+					"status":  -1,
+					"message": "subject is invalid",
+				})
+				return
+			}
+			body := context.Request.PostFormValue("body")
+			if len(body) <= 0 {
+				context.JSON(http.StatusOK, gin.H{
+					"status":  -1,
+					"message": "body is invalid",
+				})
+				return
+			}
+			files := form.File["file[]"]
+
+			m := mail.NewV3Mail()
+			name, address := parseEmailAddress(from)
+			e := mail.NewEmail(name, address)
+			m.SetFrom(e)
+			m.Subject = subject
+			p := mail.NewPersonalization()
+
+			tos := make([]*mail.Email, 0)
+			for _, _to := range to {
+				tos = append(tos, mail.NewEmail("", _to))
+			}
+			p.AddTos(tos...)
+
+			ccs := make([]*mail.Email, 0)
+			for _, _cc := range cc {
+				ccs = append(ccs, mail.NewEmail("", _cc))
+			}
+			p.AddCCs(ccs...)
+
+			bccs := make([]*mail.Email, 0)
+			for _, _bcc := range bcc {
+				bccs = append(bccs, mail.NewEmail("", _bcc))
+			}
+			p.AddBCCs(bccs...)
+
+			m.AddPersonalizations(p)
+			c := mail.NewContent("text/html", body)
+			m.AddContent(c)
+
+			for _, file := range files {
+				fileName := file.Filename
+				sourceFile, err := file.Open()
+				a := mail.NewAttachment()
+				buffer, err := ioutil.ReadAll(sourceFile)
+				if err != nil {
+					log.Println(err)
+					context.JSON(http.StatusOK, gin.H{
+						"status":  -1,
+						"message": err.Error(),
+					})
+					return
+				}
+				a.SetContent(base64.StdEncoding.EncodeToString(buffer))
+				a.SetType("application/" + filepath.Ext(fileName))
+				a.SetFilename(filepath.Base(fileName))
+				a.SetDisposition("attachment")
+				m.AddAttachment(a)
+			}
+			Body := mail.GetRequestBody(m)
+			request := sendgrid.GetRequest(configs.SendgridApiKey, "/v3/mail/send", "https://api.sendgrid.com")
+			request.Method = "POST"
+			request.Body = Body
+			response, err := sendgrid.API(request)
+			if err != nil {
+				log.Println(err)
+				context.JSON(http.StatusOK, gin.H{
+					"status":  -1,
+					"message": err.Error(),
+				})
+				return
+			} else {
+				if response.StatusCode != 202 {
+					log.Print(err)
+					context.JSON(http.StatusOK, gin.H{
+						"status":  -1,
+						"message": string(response.Body),
+					})
+					return
+				}
 			}
 			context.JSON(http.StatusOK, gin.H{
 				"status":  1,
@@ -89,56 +201,6 @@ func parseEmailAddress(email string) (name, address string) {
 			return name, address
 		}
 	}
-}
-
-func Send(from, to, subject, body string, context *gin.Context) (error) {
-	m := mail.NewV3Mail()
-	name, address := parseEmailAddress(from)
-	e := mail.NewEmail(name, address)
-	m.SetFrom(e)
-	m.Subject = subject
-	p := mail.NewPersonalization()
-	tos := []*mail.Email{mail.NewEmail("", to)}
-	p.AddTos(tos...)
-	m.AddPersonalizations(p)
-	c := mail.NewContent("text/html", body)
-	m.AddContent(c)
-	form, _ := context.MultipartForm()
-	files := form.File["file[]"]
-	for _, file := range files {
-		fileName := file.Filename
-		sourceFile, err := file.Open()
-		a := mail.NewAttachment()
-		buffer, err := ioutil.ReadAll(sourceFile)
-		if err != nil {
-			fmt.Println("Read file error: ", err)
-			return err
-		}
-		if err != nil {
-			log.Println(err)
-		} else {
-			a.SetContent(base64.StdEncoding.EncodeToString(buffer))
-			a.SetType("application/" + filepath.Ext(fileName))
-			a.SetFilename(filepath.Base(fileName))
-			a.SetDisposition("attachment")
-			m.AddAttachment(a)
-		}
-	}
-	Body := mail.GetRequestBody(m)
-	request := sendgrid.GetRequest(configs.SendgridApiKey, "/v3/mail/send", "https://api.sendgrid.com")
-	request.Method = "POST"
-	request.Body = Body
-	response, err := sendgrid.API(request)
-	if err != nil {
-		log.Println(err)
-		return err
-	} else {
-		if response.StatusCode != 202 {
-			return errors.New(string(response.Body))
-		}
-		return nil
-	}
-	return nil
 }
 
 func Logger() gin.HandlerFunc {
